b4f036edbb044ee7831b5243366427aab5b2fd4b
gitcoleli <git@cole.li>
2021-03-13 14:38:20 +0000

    FAFAD

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
-SEARCH="xxx"
-REPLACE="${CIRCLE_WORKFLOW_ID:0:8}"
+SEARCH="962906402563.dkr.ecr.us-west-2.amazonaws.com/number5:v0.1.xxx"
+REPLACE="962906402563.dkr.ecr.us-west-2.amazonaws.com/number5:v0.1.${CIRCLE_WORKFLOW_ID:0:8}"
 
 sed -i 's/$SEARCH/$REPLACE/g' deployment.yml
 
